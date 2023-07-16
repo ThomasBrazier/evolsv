73955a35b5780523d810986a8037da96508f8676
Thomas Brazier <thomas.brazier@univ-rennes1.fr>
2023-07-16 12:37:00 +0200

    Sbatch header

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#SBATCH --mail-user=thomas.brazier@univ-rennes1.fr
+#SBATCH --mail-type=all
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=120GB
+#SBATCH --time=20-60:00:00
+#SBATCH --job-name=evolsv
 
 snakemake -s pipeline.snake --use-conda --conda-frontend "mamba" --cores 8 --rerun-incomplete --stats statistiques.json
