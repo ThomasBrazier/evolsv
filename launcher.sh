f20316830118d64914743dce980b4506c626eb65
Thomas Brazier <thomas.brazier@univ-rennes1.fr>
2023-07-16 13:19:07 +0200

    Do not download data within the pipeline

--- launcher.sh
+++ launcher.sh
@@ -6,4 +6,6 @@
 #SBATCH --time=20-60:00:00
 #SBATCH --job-name=evolsv
 
+. /local/env/envsnakemake-7.28.3.sh
+
 snakemake -s pipeline.snake --use-conda --conda-frontend "mamba" --cores 8 --rerun-incomplete --stats statistiques.json
