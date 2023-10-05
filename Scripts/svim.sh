ab6df796e9dae5a8d7c053205bc9afa0e4ce9757
tbrazier <brazier.thomas@gmail.com>
2023-10-05 14:31:32 +0200

    SVIM add read names

--- Scripts/svim.sh
+++ Scripts/svim.sh
@@ -6,5 +6,5 @@ min_sv_size=$2
 min_coverage=$3
 
 # SV calling
-svim alignment $sra $sra/${sra}_sorted.bam $sra/ref_$sra.fna.gz --insertion_sequences --min_sv_size $min_sv_size --minimum_depth $min_coverage
+svim alignment $sra $sra/${sra}_sorted.bam $sra/ref_$sra.fna.gz --insertion_sequences --read_names --min_sv_size $min_sv_size --minimum_depth $min_coverage
 mv ${sra}/variants.vcf $sra/${sra}_svim.vcf
