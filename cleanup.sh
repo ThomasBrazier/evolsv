4966fc89ae1490798a7c9028bf5b7fc9eddbed1d
Thomas Brazier <thomas.brazier@univ-rennes.fr>
2025-03-07 10:33:37 +0100

    add pdflatex in Renv.yaml

--- cleanup.sh
+++ cleanup.sh
@@ -6,3 +6,6 @@ rm -rf tmp/*
 # Clean debreak intermediary outputs (lots of files)
 rm -rf data/GCA_*/debreak_minimap2/
 rm -rf data/GCA_*/debreak_ngmlr/
+
+rm -rf data-lewontin/GCA_*/debreak_minimap2/
+rm -rf data-lewontin/GCA_*/debreak_ngmlr/
